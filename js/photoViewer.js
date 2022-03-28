963d190df7d50d9f0610ddd5d0de108d4676d717
harshilhingu <91302645+harshilhingu@users.noreply.github.com>
2022-03-27 23:09:31 -0400

    completed

--- js/photoViewer.js
+++ js/photoViewer.js
@@ -1,59 +1,52 @@
-$(function () {
-    
-    var request; // Last image request
-    var $current; // Current image
-    var cache = {}; // Cache object
-    var $frame = $('#photo-frame'); // Container
-    var $thumbs = $('.thumb'); // Container
+$(function () {   
 
-    function crossfade($img) { // New image as parameter
-        if ($current) { // If image showing
-            $current.stop().fadeOut('slow'); // Stop animation & fade out
-        }
-        $img.css({ // Set CSS margins for new img
-            marginLeft: -$img.width() / 2, // Neg margin 1/2 image width
-            marginTop: -$img.height() / 2 // Neg margin 1/2 image height
-        });
-        $img.stop().fadeTo('slow', 1); // Stop animation & fade in
-        $current = $img; // New image is current one
+    $.fn.photoViewer = function () {
+        var selectedObjects = this;
+        return {
+            show: function () {
+                return $('#photo-viewer');
+            }
+        };
     }
 
-    $(document).on('click', '.thumb', function (e) { // Click on thumb
-        var $img; // Local var called $img
-        var src = this.href; // Store path to image
-        var request = src; // Store latest image
-        e.preventDefault(); // Stop default link behavior
-        $thumbs.removeClass('active'); // Remove active from thumbs
-        $(this).addClass('active'); // Add active to clicked one
-        $frame.removeClass("default-text");
-        if (cache.hasOwnProperty(src)) { // If cache contains this img
-            if (cache[src].isLoading === false) { // and it's not loading
-                crossfade(cache[src].$img); // Call crossfade() function
-            }
-        } else { // Otherwise it is not in the cache
-            $img = $('<img/>'); // Store empty <img/> in $img
-            cache[src] = { // Store this image in cache
-                $img: $img, // Add the path to the image
-                isLoading: true // Set isLoading to false
-            };
-        
-            // When image has loaded this code runs
-            $img.on('load', function () { // When image loaded
-                $(this).hide(); // Hide it
-                // Remove is-loading class & append image
-                $frame.removeClass('is-loading').append($img);
-                cache[src].isLoading = false; // Update isLoading in cache
-                // If still most recently requested image then
-                if (request === src) {
-                    crossfade($(this)); // Call crossfade() function
-                } // to solve async load issue
-            });
-            $frame.addClass('is-loading'); // Add is-loading to frame
-            $img.attr({ // Set attributes on <img>
-                'src': src, // src attribute loads image\
-                'alt': this.title || '' // Add title if one given
-            });
-        }
+
+    $('#photo-viewer').photoViewer().show().on('click', 'photo-box', function (e) {
+        e.preventDefault();
+        console.log("test", this);
+        var $content = $(this).clone().find('img').css({
+            marginLeft: 0,
+            marginTop: 0,
+            width: '100%',
+            height: 'auto',
+            border: '1px solid black',
+        });
+
+        var modal = new Modal();
+        modal.open({
+            content: $content,
+            width: 800,
+            height: 450
+        });
     });
+
+
+});
+
+$(function () {
+    $('.thumbnail-anchor active').click(function (e) {
+        e.preventDefault();
+        var href = e.target.parentNode.href;
+        var hrefvalue = href.split('/')
+        var photoframe = hrefvalue[hrefvalue.length - 1]
+        $(".photo-box").find("img").remove();
+        $(".photo-box").append("<img id='spinner'  src='img/load.gif' style='width: 800px!important; height: 450px!important; border: 1px solid black' alt=''>")
+        setTimeout(function () {
+            $("#spinner").remove();
+            $(".photo-box").append('<img src="' + `img/${photo-box}` + '">');
+            $("a.photo-box").attr('href', `img/${photo-box}`);
+        }, 2000)
+
+    });
+
 
 });
